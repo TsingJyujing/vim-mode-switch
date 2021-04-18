ef18e68b5f1f2d5d06ccf8185aba8b3572cd4bc2
Yuan Yifan <yifan.yuan@linecorp.com>
2021-04-18 23:40:00 +0900

    Add pedal solution

---
+++ pedal-solution/README.md
@@ -0,0 +1,22 @@
+# Pedal Solution
+
+**This solution is still WIP, I'm verifing this.**
+
+## How to use
+
+Using Padel to control vim mode.
+
+- while pressed the padel 1, send a "I" to computer and send a ESC key after you released.
+- while pressed the padel 2, send a "V" to computer and send a ESC key after you released.
+
+Just like the brake and throttle in the vehicle, you should use right foot (if you haven't lost it) to control it for avoiding press them at same time.
+
+
+## References
+
+- [arduino keyboard library](https://www.arduino.cc/reference/en/language/functions/usb/keyboard/)
+    - These core libraries allow the 32u4 and SAMD based boards **(Leonardo, Esplora, Zero, Due and MKR Family)** to appear as a native Mouse and/or Keyboard to a connected computer.
+    - I'm using [KKHMF](https://www.amazon.co.jp/gp/product/B081DY1NWW), but I think using taobao you can buy cheaper boards...
+- For pedal, I'm using YDT1-16, any switch is OK.
+    - https://detail.tmall.com/item.htm?id=9350412322
+    - https://www.amazon.co.jp/gp/product/B08KXMM3VM
---
+++ pedal-solution/vim_pedal.ino
@@ -0,0 +1,89 @@
+#include <Keyboard.h>
+
+/**
+ * STATUS list
+ * 0: Normal status, waiting for press
+ * 1/2: Insert/View mode, waiting for release Button I/V
+ */
+unsigned short STATUS_INSERT = 0;
+// Removing joggle for pushing button
+
+const int INSERT_MODE_BUTTON = 2; // Pin of I Mode
+const int VIEW_MODE_BUTTON = 3;   // Pin of I Mode
+
+void setup()
+{
+  // Set pull up (INPUT) mode for D2 and D3
+  pinMode(INSERT_MODE_BUTTON, INPUT_PULLUP);
+  pinMode(VIEW_MODE_BUTTON, INPUT_PULLUP);
+  Keyboard.begin();
+}
+
+// Press single key
+void pressSingleKey(char key, unsigned int delayMs)
+{
+  Keyboard.press(key);
+  delay(delayMs);
+  Keyboard.release(key);
+}
+
+// Press single key
+void pressSingleKey(char key)
+{
+  pressSingleKey(key, 100);
+}
+
+const unsigned int JOGGLE_REMOVING = 100; //ms
+/**
+ * Check pin (button) status without joggling
+ */
+bool checkButtonStatus(int pin, bool expectedStatus)
+{
+  if (digitalRead(pin) == expectedStatus)
+  {
+    delay(JOGGLE_REMOVING);
+    if (digitalRead(pin) == expectedStatus)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  if (STATUS_INSERT == 0)
+  {
+    if (checkButtonStatus(INSERT_MODE_BUTTON, LOW))
+    {
+      STATUS_INSERT = 1;
+      pressSingleKey('I');
+    }
+    else if (checkButtonStatus(VIEW_MODE_BUTTON, LOW))
+    {
+      STATUS_INSERT = 2;
+      pressSingleKey('V');
+    }
+  }
+  else if (STATUS_INSERT == 1)
+  {
+    if (checkButtonStatus(INSERT_MODE_BUTTON, HIGH))
+    {
+      pressSingleKey(KEY_ESC);
+    }
+  }
+  else if (STATUS_INSERT == 2)
+  {
+    if (checkButtonStatus(VIEW_MODE_BUTTON, HIGH))
+    {
+      pressSingleKey(KEY_ESC);
+    }
+  }
+  else
+  {
+    // Should not be here.
+    STATUS_INSERT = 0;
+    setup();
+  }
+}
